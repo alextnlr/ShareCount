b3588933bc2a9546b29b6a3f654a03a81245246f
timeo <timeo.jacquier@gmail.com>
2021-11-10 14:10:15 +0100

    compte

---
+++ Compte.cpp
@@ -0,0 +1,5 @@
+//
+// Created by timeo on 10/11/2021.
+//
+
+include "Compte.h"
---
+++ Compte.h
@@ -0,0 +1,14 @@
+//
+// Created by timeo on 10/11/2021.
+//
+
+#ifndef SHARECOUNT_COMPTE_H
+#define SHARECOUNT_COMPTE_H
+
+
+class Compte {
+
+};
+
+
+#endif //SHARECOUNT_COMPTE_H
