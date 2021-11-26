0c055349b14e3038595a3ad5db094b46a37ee6c2
timeo <timeo.jacquier@gmail.com>
2021-11-26 22:57:16 +0100

    ajout modèle cagnotte

---
+++ cagnotte.cpp
@@ -0,0 +1,36 @@
+#include "cagnotte.h"
+
+using namespace std;
+
+Cagnotte::Cagnotte(std::string nom, int idCagnotte, Compte* Createur)
+{
+    m_nom = nom;
+    m_idCagnotte = idCagnotte;
+    m_comptes[Createur->getIdentifiant()] = Createur;
+    m_idCreateur = Createur->getIdentifiant();
+    m_budget = 0;
+}
+
+void Cagnotte::addParticipant(Compte* compte){
+    m_comptes[compte->getIdentifiant()] = compte;
+}
+
+void Cagnotte::addBudget(int toAdd){
+    m_budget += toAdd;
+}
+
+string Cagnotte::getNom(){
+    return m_nom;
+}
+
+int Cagnotte::getIdCagnotte(){
+    return m_idCagnotte;
+}
+
+int Cagnotte::getIdCreateur(){
+    return m_idCreateur;
+}
+
+int Cagnotte::getBudget(){
+    return m_budget;
+}
---
+++ cagnotte.h
@@ -0,0 +1,112 @@
+#ifndef CAGNOTTE_H
+#define CAGNOTTE_H
+#include <iostream>
+#include "Compte.h"
+#include <map>
+
+class Cagnotte
+{
+public:
+    /**
+     * @brief Constructeur par défaut
+     * @param nom nom de la cagnoote
+     * @param idCagnotte id unique identifiant la cagnotte
+     * @param Createur compte créateur de la cagnotte
+     */
+    Cagnotte(std::string nom, int idCagnotte, Compte* Createur);
+
+    /**
+     * @brief getNom retourne le nom de la cagnotte
+     * @return retourne le nom de la cagnotte
+     */
+    std::string getNom();
+
+    /**
+     * @brief getIdCagnotte retourne l'id de la cagnotte
+     * @return id de la cagnotte
+     */
+    int getIdCagnotte();
+
+    /**
+     * @brief getIdCreateur retourne l'id du créateur de la cagnotte
+     * @return id du créateur de la cagnotte
+     */
+    int getIdCreateur();
+
+    /**
+     * @brief getBudget retourne le budget actuel de la cagnotte
+     * @return budget actuel de la cagnotte
+     */
+    int getBudget();
+
+    /**
+     * @brief getNbParticipant retourne le nombre de participant à la cagnotte
+     * @return nombre de participant à la cagnotte
+     */
+    int getNbParticipant();
+
+    /**
+     * @brief getParticipant retourne un compte participant à la cagnotte, identifié par son identifiant
+     * @param id identifiant du compte à retourner
+     * @return Compte identifié par l'id passé en paramètres
+     */
+    Compte* getParticipant(int id);
+
+    /**
+     * @brief getListeParticipant retourne la liste complète des participants
+     * @return map complète des participants
+     */
+    std::map<int, Compte*> getListeParticipant();
+
+    /**
+     * @brief addParticipant Ajoute un participant à la cagnotte
+     * @param compte Compte à ajouter à la cagnotte
+     */
+    void addParticipant(Compte* compte);
+
+    /**
+     * @brief addBudget Ajoute de l'argent à la cagnotte
+     * @param toAdd somme d'argent à ajouter
+     */
+    void addBudget(int toAdd);
+
+    /**
+     * Destructeur
+     */
+    ~Cagnotte();
+
+
+private:
+    /**
+     * @brief m_nom nom de la cagnotte
+     */
+    std::string m_nom;
+
+    /**
+     * @brief m_idCagnotte identifiant de la cagnotte
+     */
+    int m_idCagnotte;
+
+    /**
+     * @brief m_idCreateur identifiant du créateur de la cagnotte
+     */
+    int m_idCreateur;
+
+    /**
+     * @brief m_budget budget actuel de la cagnotte
+     */
+    int m_budget;
+
+    /**
+     * @brief m_comptes map des comptes liés à la cagnotte
+     */
+    std::map<int, Compte*> m_comptes;
+
+    /**
+     * @brief estOuvert Indique si la cagnotte est ouverte ou fermée
+     */
+    bool estOuvert;
+
+};
+
+#endif // CAGNOTTE_H
